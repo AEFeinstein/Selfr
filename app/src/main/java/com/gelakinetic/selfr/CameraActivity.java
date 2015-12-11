740496d98057430c921882b17a54010f9f223088
Adam <gelakinetic@gmail.com>
2015-12-11 01:03:09 -0500

    Progress towards a real "about" dialog

--- app/src/main/java/com/gelakinetic/selfr/CameraActivity.java
+++ app/src/main/java/com/gelakinetic/selfr/CameraActivity.java
@@ -34,6 +34,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -381,8 +382,17 @@ public class CameraActivity extends AppCompatActivity implements IAudioReceiver 
         mToolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
-                Toast.makeText(CameraActivity.this, "Version Info", Toast.LENGTH_SHORT).show();
+                // TODO fill this with useful information
+                AlertDialog.Builder builder = new AlertDialog.Builder(CameraActivity.this);
+                String title;
+                try {
+                    title = getString(R.string.app_name) + " " +
+                            getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+                } catch (PackageManager.NameNotFoundException e) {
+                    title = getString(R.string.app_name);
+                }
+                builder.setMessage(R.string.about_message).setTitle(title);
+                builder.create().show();
             }
         });
     }
@@ -430,7 +440,7 @@ public class CameraActivity extends AppCompatActivity implements IAudioReceiver 
             /* Set up the camera */
             mCamera = getCameraInstance(mCameraType);
             if (mCamera != null) {
-                mCameraPreview = new CameraPreview(this, mCamera); // todo null checks for this guy
+                mCameraPreview = new CameraPreview(this, mCamera);
                 mContentView.addView(mCameraPreview);
             }
 
